64d073648a84af85666513c5add35c64cad45a1d
thiago-fullenbach <thiago.fullenbach@hotmail.com>
2021-02-07 16:37:25 -0300

    c10 - Adiciona roteamento para o componente principal

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { NoteReadComponent } from './components/view/note-read/note-read.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NoteReadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
